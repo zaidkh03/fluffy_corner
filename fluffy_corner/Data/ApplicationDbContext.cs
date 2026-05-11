97ba8a93a5b1aadf416855e972651804c044b88e
Zaid Alkhalili <alkhalili208@gmail.com>
2026-05-11 11:12:50 +0300

    Add project files.

---
+++ fluffy_corner/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using fluffy_corner.Models;
+
+
+namespace fluffy_corner.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
