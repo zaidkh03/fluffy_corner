6a03064ef9e609359d239d426a95aede21a7c60a
Hala Albasaitah <your_email@example.com>
2026-05-11 11:42:18 +0300

    add first 4 models

---
+++ fluffy_corner/Models/ProductImage.cs
@@ -0,0 +1,19 @@
+﻿namespace fluffy_corner.Models
+{
+    public class ProductImage
+    {
+        public int Id { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public bool IsPrimary { get; set; } = false;
+
+        public int SortOrder { get; set; } = 0;
+
+        // Foreign Key
+        public int ProductId { get; set; }
+
+        // Navigation
+        public Product Product { get; set; }
+    }
+}
